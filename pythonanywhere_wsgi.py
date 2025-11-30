f944eda25c12efbb44aa6ea043a8d91a937b444d
Shreya Suryavanshi <shreyasuryavanshi6169@gmail.com>
2025-11-30 15:57:02 +0530

    Fix WSGI and remove whitenoise from middleware

--- pythonanywhere_wsgi.py
+++ pythonanywhere_wsgi.py
@@ -10,7 +10,7 @@ project_home = '/home/shreya-suryavanshi/poll_project'
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'poll_project.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poll_project.settings')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
